bf1617ada807eb5c2cc8e1d162cabf4378c897cb
yuinin <yuis2teddy@gmail.com>
2022-09-11 22:23:10 +0900

    migrateファイルの修正

--- db/migrate/20220906070059_devise_create_users.rb
+++ db/migrate/20220906070059_devise_create_users.rb
@@ -7,9 +7,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name, null: false, default: ""
-      t.text :profile, null: false,
-      t.text :occupation, null: false,
-      t.text :position, null: false,
+      t.text :profile, null: false
+      t.text :occupation, null: false
+      t.text :position, null: false
 
       ## Recoverable
       t.string   :reset_password_token
